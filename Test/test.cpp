e88ab6ebde830fc00d51857c17ca467e57c74cd4
He Jiahui <914234474@qq.com>
2022-04-04 23:37:12 +0800

    Add new comment

--- Test/test.cpp
+++ Test/test.cpp
@@ -21,32 +21,37 @@
 #include "unordered_set_test.h"
 #include "string_test.h"
 
-int main()
-{
-  using namespace mystl::test;
+int main() {
+    using namespace mystl::test;
 
-  std::cout.sync_with_stdio(false);
+    // C++为了与C兼容，让IO的时候不发生混乱，将输出流绑定在一起，会多处很多不必要的操作
+    // 此操作将绑定接触，提高IO执行效率
+    std::cout.sync_with_stdio(false);
 
-  RUN_ALL_TESTS();
-  algorithm_performance_test::algorithm_performance_test();
-  vector_test::vector_test();
-  list_test::list_test();
-  deque_test::deque_test();
-  queue_test::queue_test();
-  queue_test::priority_test();
-  stack_test::stack_test();
-  map_test::map_test();
-  map_test::multimap_test();
-  set_test::set_test();
-  set_test::multiset_test();
-  unordered_map_test::unordered_map_test();
-  unordered_map_test::unordered_multimap_test();
-  unordered_set_test::unordered_set_test();
-  unordered_set_test::unordered_multiset_test();
-  string_test::string_test();
+    // 运行所有测试案例
+    RUN_ALL_TESTS();
+
+    // 测试案例1：函数性能测试
+    // algorithm_performance_test是命名空间，在该命名空间中调用algorithm_performance_test()函数
+    algorithm_performance_test::algorithm_performance_test();
+    vector_test::vector_test();
+    list_test::list_test();
+    deque_test::deque_test();
+    queue_test::queue_test();
+    queue_test::priority_test();
+    stack_test::stack_test();
+    map_test::map_test();
+    map_test::multimap_test();
+    set_test::set_test();
+    set_test::multiset_test();
+    unordered_map_test::unordered_map_test();
+    unordered_map_test::unordered_multimap_test();
+    unordered_set_test::unordered_set_test();
+    unordered_set_test::unordered_multiset_test();
+    string_test::string_test();
 
 #if defined(_MSC_VER) && defined(_DEBUG)
-  _CrtDumpMemoryLeaks();
+    _CrtDumpMemoryLeaks();
 #endif // check memory leaks
 
 }
